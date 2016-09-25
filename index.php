869f6c861e999a5785c1c61848e16de9d3b84624
Kouceyla <hadjikouceyla@gmail.com>
2016-09-25 12:03:41 +0100

    Ajout et modifier style.css
    Add menu
    
    Add Footer
    
    Add paragraphe
    
    WIP

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 ma 1er ligne
 une nouvelle ligne
+fix bug1
+ajouter fonctionnalité 1
+
+<ul>
+	<li>Home</li>
+	<li>About</li>
+	<li>contact</li>
+</ul>
+
+<p> ipsum dolor sit amet, consectetur adipisicing elit. Impedit id porro sapiente ratione odit tempore unde, ipsam laboriosam atque sed optio sequi, magnam velit exercitationem deserunt nostrum, sit distinctio perspiciatis.</p>
+
+<footer>
+	coptight 2016
+</footer>
