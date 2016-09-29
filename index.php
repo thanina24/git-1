6b5db48f62574d92854608a1fadf9f0ed1293453
thanina <belhocine.thanina@hotmail.fr>
2016-09-29 10:50:50 +0200

    ajout lorem dans index

--- index.php
+++ index.php
@@ -9,4 +9,10 @@ ajouter fonctionnalité 1
 	<li>contact</li>
 </ul>
 
-<p> ipsum dolor sit amet, consectetur adipisicing elit. Impedit id porro sapiente ratione odit tempore unde, ipsam laboriosam atque sed optio sequi, magnam velit exercitationem deserunt nostrum, sit distinctio perspiciatis.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
